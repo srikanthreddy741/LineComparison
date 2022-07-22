89ca4bf847c33bdc4e2a8bf0f52890cac840d96b
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-22 22:21:02 +0530

    Added WellCome Mesg

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WellCome To LineComparison");
+        }
+    }
+}
